22f2070ee37d0a79e068c8c6b5b8b1acf28c4382
yjwx0017 <761225349@qq.com>
2016-11-07 23:33:30 +0800

    Update Label.cpp

--- duilib2/src/Controls/Label.cpp
+++ duilib2/src/Controls/Label.cpp
@@ -5,7 +5,32 @@ namespace duilib2
 
 static String gLabelProperties[][3] =
 {
-	// name              value         type
+	// name               value             type
+	{"align",             "left",           "String"},
+	{"endellipsis",       "false",          "Bool"},
+	{"endellipsis",       "false",          "Bool"},
+	{"font",              "-1",             "Int"},
+	{"textcolor",         "0x00000000",     "Color"},
+	{"disabledtextcolor", "0x00000000",     "Color"},
+	{"textpadding",       "0,0,0,0",        "Rect"},
+	{"showhtml",          "false",          "Bool"},
+	{"enabledeffect",     "false",          "Bool"},
+	{"align",             "left",           "String"},
+	{"valign",            "center",         "String"},
+	{"rhaa",              "0",              "Int"},
+	{"enabledstroke",     "false",          "Bool"},
+	{"strokecolor",       "0x00000000",     "Color"},
+	{"transstroke",       "255",            "Int"},
+	{"enabledshadow",     "false",          "Bool"},
+	{"gradientangle",     "0",              "Int"},
+	{"gradientlength",    "0",              "Int"},
+	{"textcolor1",        "0x00000000",     "Color"},
+	{"textshadowcolora",  "0xff000000",     "Color"},
+	{"textshadowcolorb",  "0xff000000",     "Color"},
+	{"transshadow",       "100",            "Int"},
+	{"transshadow1",      "100",            "Int"},
+	{"transtext",         "100",            "Int"},
+	{"transtext1",        "100",            "Int"},
 	{"", "", ""}
 };
 
