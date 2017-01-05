06a4e0f339f177cbcc90691d6831912339369a07
yjwx0017 <761225349@qq.com>
2017-01-05 19:26:32 +0800

    Update class Image

--- duilib2/include/Image.h
+++ duilib2/include/Image.h
@@ -2,6 +2,7 @@
 #define IMAGE_H
 
 #include <duilib2_global.h>
+#include <PropertyTypes.h>
 
 namespace duilib2
 {
@@ -11,6 +12,41 @@ class DUILIB2SHARED_EXPORT Image
 public:
 	Image();
 	~Image();
+
+	String getFileName() const;
+	String getRes() const;
+	String getResType() const;
+	Rect   getDest() const;
+	Rect   getSource() const;
+	Color  getMaskColor() const;
+	int    getFadeValue() const;
+	bool   isHole() const;
+	bool   isXTiled() const;
+	bool   isYTiled() const;
+
+	void setFileName(const String& filename);
+	void setRes(const String& res);
+	void setResType(const String& resType);
+	void setDest(const Rect& rect);
+	void setSource(const Rect& rect);
+	void setMaskColor(const Color& color);
+	void setFadeValue(int value);
+	void setIsHole(bool value);
+	void setIsXTiled(bool value);
+	void setIsYTiled(bool value);
+
+private:
+	String mFileName; // file='aaa.jpg'
+	String mRes;      // res=''
+	String mResType;  // restype='0'
+	Rect   mDest;     // dest='0,0,0,0'
+	Rect   mSource;   // source='0,0,0,0'
+	Rect   mCorner;   // corner='0,0,0,0'
+	Color  mMask;     // mask='#FF0000'
+	int    mFade;     // fade='255'
+	bool   mHole;     // hole='false'
+	bool   mXTiled;   // xtiled='false'
+	bool   mYTiled;   // ytiled='false'
 };
 
 } // namespace duilib2
--- duilib2/src/Image.cpp
+++ duilib2/src/Image.cpp
@@ -13,5 +13,106 @@ Image::~Image()
 
 }
 
+String Image::getFileName() const
+{
+	return mFileName;
+}
+
+String Image::getRes() const
+{
+	return mRes;
+}
+
+String Image::getResType() const
+{
+	return mResType;
+}
+
+Rect Image::getDest() const
+{
+	return mDest;
+}
+
+Rect Image::getSource() const
+{
+	return mSource;
+}
+
+Color Image::getMaskColor() const
+{
+	return mMask;
+}
+
+int Image::getFadeValue() const
+{
+	return mFade;
+}
+
+bool Image::isHole() const
+{
+	return mHole;
+}
+
+bool Image::isXTiled() const
+{
+	return mXTiled;
+}
+
+bool Image::isYTiled() const
+{
+	return mYTiled;
+}
+
+void Image::setFileName(const String& filename)
+{
+	mFileName = filename;
+}
+
+void Image::setRes(const String& res)
+{
+	mRes = res;
+}
+
+void Image::setResType(const String& resType)
+{
+	mResType = resType;
+}
+
+void Image::setDest(const Rect& rect)
+{
+	mDest = rect;
+}
+
+void Image::setSource(const Rect& rect)
+{
+	mSource = rect;
+}
+
+void Image::setMaskColor(const Color& color)
+{
+	mMask = color;
+}
+
+void Image::setFadeValue(int value)
+{
+	mFade = value;
+}
+
+void Image::setIsHole(bool value)
+{
+	mHole = value;
+}
+
+void Image::setIsXTiled(bool value)
+{
+	mXTiled = value;
+}
+
+void Image::setIsYTiled(bool value)
+{
+	mYTiled = value;
+}
+
+
 } // namespace duilib2
 
